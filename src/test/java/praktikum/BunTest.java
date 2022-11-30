4dd96568576626411f96eb7d343b4a3f8a63349f
Irina Ustelemova <irinaus22@mail.ru>
2022-11-30 17:57:04 +0400

    Add BunTest and IngredientTest and jacoco

---
+++ src/test/java/praktikum/BunTest.java
@@ -0,0 +1,25 @@
+package praktikum;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class BunTest {
+
+    private String name = "Suoperrrr";
+    private float price = (float) 2.22;
+
+    @Test
+    public void BunGetNameTest() {
+        Bun bun = new Bun(name,price);
+        assertEquals(name,bun.getName());
+    }
+
+    @Test
+    public void BunGetPriceTest() {
+        Bun bun = new Bun(name,price);
+        Assert.assertEquals(price,bun.getPrice(), 0.0f);
+    }
+
+}
